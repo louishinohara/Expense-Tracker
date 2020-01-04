974a43f4a493289b75f8e7e386092a999e13ceec
Ushio Shinohara <44354821+louishinohara@users.noreply.github.com>
2020-01-04 07:36:26 -0500

    Adding Files

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch, 
+          children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Container(
+              width: double.infinity,
+              child: Text('Chart'),
+            ),
+            elevation: 5,
+          ),
+          Card(
+            color: Colors.green,
+            child: Text('List of TX'),
+          )
+        ]));
+  }
+}
